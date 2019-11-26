1a5a6ad7148b05ab0c96d4ffbcec4b2b61e1c3aa
Schomer <ischomer@vols.utk.edu>
2019-11-26 07:52:57 -0500

    - wrapped code to except a KeyboardInterrupt - more usability for storing and loading results for continued training over multiple runs

--- othello_rl.py
+++ othello_rl.py
@@ -17,7 +17,7 @@ class OthelloAgent:
         self.tile = 'X'
         self.memory = deque(maxlen=2000)
         self.gamma = 1.0  # episodic --> undiscounted
-        self.epsilon = .20
+        self.epsilon = .10
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.9995
         self.learning_rate = 0.01
@@ -65,8 +65,9 @@ class OthelloAgent:
             target_NN[0][action] = target   # only this Q val will be updated
             self.model.fit(state, target_NN, epochs=1, verbose=0)
         # optional epsilon decay feature
+        # TODO - fix this to work properly as linear decay
         if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+            self.epsilon -= (self.epsilon - self.epsilon_min)/episodes
 
     def load(self, name):
         self.model.load_weights(name)
@@ -75,74 +76,88 @@ class OthelloAgent:
         self.model.save_weights(name)
 
 
-if __name__ == "__main__":
-    storing = False
-    # initialize agent and environment
-    agent = OthelloAgent()
-    game = OthelloGame(interactive=False, show_steps=False)
-
-    # FILENAME CONVENTION
-    #      'saves/NN-type_opponent_num-episodes'
-    if storing:
-        save_filename = 'final_project/saves/basic-sequential_rand_2000_2'
-        load_filename = 'final_project/saves/basic-sequential_rand_2000'
-        agent.load(load_filename + ".h5")
-
-    terminal = False
-    batch_size = 32
-    episodes = 2000
-    avg_result = 0
-    results_over_time = np.zeros(episodes)
-
-    # time it
-    t_start = process_time()
-    for e in range(episodes):
-        game.reset()
-        game.start()
-        state = game.get_state()  # 8x8 numpy array
-        state = np.reshape(state, [1, 64])   # 1x64 vector
-
-        for move in range(100):   # max amount of moves in an episode
-            action = agent.get_action(state)
-            reward, next_state, terminal = game.step(action)
-            next_state = np.reshape(next_state, [1, 64])
-            agent.remember(state, action, reward, next_state, terminal)
-            state = next_state
-            if terminal:
-                # terminal reward is 0 for loss, 0.5 for tie, 1 for win
-                # use this as an indexing code to get the result
-                outcomes = ['Loss', 'Tie', 'Win']
-                result = outcomes[int(reward*2)]
-                if result == 'Win':
-                    n = 1
-                else:
-                    n = 0
-                avg_result += (1/(e+1))*(n - avg_result)
-                results_over_time[e] = avg_result
-                print("episode {}: {} moves, Result: {}, e: {:.2}"
-                      .format(e, move, result, agent.epsilon))
-                print("Average win/loss ratio: ", avg_result)
-                break
-            # Question - maybe only update every batch_size moves
-            #       (instead of every move after batch_size)?
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
-
-        if e % 100 == 0 and e > 0 and storing:
-            # save name as 'saves/model-type_training-opponent_num-episodes.h5'
-            agent.save(save_filename + ".h5")
-
+def store_results():
     # present the timed results
     t_stop = process_time()
-    print('Runtime: {}.'.format(t_stop-t_start))
-
+    print('Runtime: {}.'.format(t_stop - t_start))
     # create and save a figure
     if storing:
-        t = [i for i in range(episodes)]
+        t = [i for i in range(len(results_over_time))]
         fig = plt.figure()
         ax = fig.add_subplot(111)
         ax.plot(t, results_over_time)
         ax.set_xlabel("Episode")
         ax.set_title("Percent Wins During Training")
         plt.savefig(save_filename + '.png')
-        np.save(save_filename + '.npy', results_over_time)
+
+
+if __name__ == "__main__":
+    try:
+        storing = True
+        loading = False
+        # initialize agent and environment
+        agent = OthelloAgent()
+        game = OthelloGame(interactive=False, show_steps=False)
+
+        # FILENAME CONVENTION
+        #      'saves/NN-type_opponent_num-episodes'
+        if storing:
+            save_filename = 'final_project/saves/basic-sequential_rand_20000'
+            load_filename = 'final_project/saves/basic-sequential_rand_20000_3'
+        if loading:
+            agent.load(load_filename + ".h5")
+
+        terminal = False
+        batch_size = 32
+        episodes = 20000
+        if loading:
+            prev_data = np.load(load_filename + '.npy')
+            avg_result = prev_data[-1]
+            episode_start = len(prev_data)
+            results_over_time = np.append(prev_data, np.zeros(episodes))
+        else:
+            avg_result = 0
+            results_over_time = np.zeros(episodes)
+            episode_start = 0
+
+        # time it
+        t_start = process_time()
+        for e in range(episode_start, episode_start + episodes):
+            game.reset()
+            game.start()
+            state = game.get_state()  # 8x8 numpy array
+            state = np.reshape(state, [1, 64])   # 1x64 vector
+
+            for move in range(100):   # max amount of moves in an episode
+                action = agent.get_action(state)
+                reward, next_state, terminal = game.step(action)
+                next_state = np.reshape(next_state, [1, 64])
+                agent.remember(state, action, reward, next_state, terminal)
+                state = next_state
+                if terminal:
+                    # terminal reward is 0 for loss, 0.5 for tie, 1 for win
+                    # use this as an indexing code to get the result
+                    outcomes = ['Loss', 'Tie', 'Win']
+                    result = outcomes[int(reward*2)]
+                    if result == 'Win':
+                        n = 1
+                    else:
+                        n = 0
+                    avg_result += (1/(e+1))*(n - avg_result)
+                    results_over_time[e] = avg_result
+                    print("episode {}: {} moves, Result: {}, e: {:.2}"
+                          .format(e, move, result, agent.epsilon))
+                    print("Average win/loss ratio: ", avg_result)
+                    break
+                # Question - maybe only update every batch_size moves
+                #       (instead of every move after batch_size)?
+                if len(agent.memory) > batch_size:
+                    agent.replay(batch_size)
+
+            if e % 100 == 0 and e > 0 and storing:
+                # save name as 'saves/model-type_training-opponent_num-episodes.h5'
+                agent.save(save_filename + ".h5")
+                np.save(save_filename + '.npy', results_over_time)
+        store_results()
+    except KeyboardInterrupt:
+        store_results()
